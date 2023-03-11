ff2039c4416cf8ee380e7cc469596464af4d1e1d
Sukera27 <rafa_mancina@hotmail.com>
2023-03-11 10:47:35 +0100

    ejercicio3 js

--- js/js.js
+++ js/js.js
@@ -22,3 +22,54 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  /*ejercicio 3*/
+const nombreInput = document.getElementById('nombre');
+        const emailInput = document.getElementById('email');
+        const descripcionInput = document.getElementById('descripcion');
+
+        function actualizarEstadoInput(input) {
+            if (input.value.trim() === '') {
+                input.classList.remove('valido');
+                input.classList.remove('invalido');
+            } else if (input.classList.contains('invalido')) {
+                input.classList.remove('invalido');
+                input.classList.add('valido');
+            } else {
+                input.classList.add('valido');
+            }
+        }
+
+        function actualizarNombre() {
+            actualizarEstadoInput(nombreInput);
+            if (nombreInput.classList.contains('valido')) {
+                nombreInput.style.borderColor = '#00ff00';
+            } else {
+                nombreInput.style.borderColor = '#ff0000';
+            }
+        }
+
+        function actualizarEmail() {
+            actualizarEstadoInput(emailInput);
+            if (emailInput.checkValidity()) {
+                emailInput.style.borderColor = '#00ff00';
+            } else {
+                emailInput.style.borderColor = '#ff0000';
+            }
+        }
+
+        function actualizarDescripcion() {
+            actualizarEstadoInput(descripcionInput);
+            if (descripcionInput.classList.contains('valido')) {
+                descripcionInput.style.borderColor = '#00ff00';
+            } else {
+                descripcionInput.style.borderColor = '#ff0000';
+            }
+        }
+
+        nombreInput.addEventListener('input', actualizarNombre);
+        emailInput.addEventListener('input', actualizarEmail);
+        descripcionInput.addEventListener('input', actualizarDescripcion);
+
+        nombreInput.addEventListener('blur', actualizarNombre);
+        emailInput.addEventListener('blur', actualizarEmail);
+        descripcionInput.addEventListener('blur', actualizarDescripcion);
