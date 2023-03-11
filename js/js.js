b261fcd58ec5d8058d1b5c37a9a5737c63fa7510
Sukera27 <rafa_mancina@hotmail.com>
2023-03-11 10:44:51 +0100

    ejercicio2 js

--- js/js.js
+++ js/js.js
@@ -3,3 +3,22 @@ function cambiotema(){
     var seleccionar=document.getElementById("colores").value;
     document.body.style.backgroundColor=seleccionar;
 }
+/*ejercicio 2*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const select = document.getElementById('modulo');
+    const container = document.querySelector('#filtrar');
+    const cards = document.querySelectorAll('[data-categoria]');
+  
+    select.addEventListener('change', () => {
+      const valorSelect = select.value;
+      cards.forEach((card) => {
+        const categorias = card.dataset.categoria.split(' ');
+        if (valorSelect === 'all' || categorias.includes(valorSelect)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+  });
